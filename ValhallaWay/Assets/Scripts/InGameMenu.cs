c03146de6e2aeb481ef140597b1bc7b6437aaaf6
Caio Marchi <caio@i9acao.com.br>
2018-08-26 11:23:41 -0300

    Menus implementados. Build jogável criada

---
+++ ValhallaWay/Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InGameMenu : MonoBehaviour {
+
+    public Button MainMenu;
+
+    void Start() {
+        MainMenu.onClick.AddListener(delegate () {
+            SceneManager.LoadScene("Main_Menu");
+        });
+    }
+}
