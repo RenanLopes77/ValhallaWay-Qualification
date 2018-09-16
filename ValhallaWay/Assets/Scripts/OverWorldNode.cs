764e619da4ef1beabcedc5780c45e3d1cee27e70
Caio Marchi <caio@gmail.com>
2018-09-16 17:29:39 -0300

    Overworld Nivel 0 criado; navegação n0 implementada

---
+++ ValhallaWay/Assets/Scripts/OverWorldNode.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverWorldNode : MonoBehaviour {
+
+    public List<OverWorldNode> linkedNodes;
+    OverWorldNode [] directions;
+    public OverWorldNode UP;
+    public OverWorldNode RIGHT;
+    public OverWorldNode DOWN;
+    public OverWorldNode LEFT;
+
+    private void Awake() {
+        directions = new OverWorldNode[4];
+        directions[0] = UP;
+        directions[1] = RIGHT;
+        directions[2] = DOWN;
+        directions[3] = LEFT;
+    }
+
+    public bool IsLinkedTo(OverWorldNode otherNode) {
+        if (linkedNodes.Contains(otherNode)) {
+            return true;
+        }
+        return false;
+    }
+
+    public bool CanMoveTo(Utils.Direction dir) {
+        if (directions[(int)dir] != null) {
+            return true;
+        }
+        
+        return false;
+    }
+
+
+}
---
+++ ValhallaWay/Assets/Scripts/Utils.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class Utils {
+    public enum Direction {
+        UP, RIGHT, DOWN, LEFT
+    }
+	
+}
