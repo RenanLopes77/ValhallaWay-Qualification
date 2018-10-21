81134a79df48cee5ee2d53657d28be48ec36949f
Renan Lopes <renan-lopes123@hotmail.com>
2018-10-21 13:25:06 -0300

    Scene selection script

---
+++ ValhallaWay/Assets/Scripts/Utils/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class Utils {
+    public enum Direction {
+        DOWN,
+        LEFT,
+        RIGHT,
+        UP,
+    }
+
+    public enum Scenes
+    {
+        Null,
+        Credits,
+        MainMenu,
+        Midgard,
+        Settings,
+    }
+}
