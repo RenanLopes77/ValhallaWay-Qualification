7c08b1c5dcdc83b76743389946a98fcd5a6845ed
Renan Lopes <renan-lopes123@hotmail.com>
2018-12-11 00:48:10 -0200

    Checkpoints

---
+++ ValhallaWay/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+	public SpriteRenderer Flag;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			other.GetComponent<PlayerController>().LastCheckpoint = gameObject;
+			Flag.enabled = true;
+		}
+	}
+}
