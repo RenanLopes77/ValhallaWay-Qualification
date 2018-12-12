687e790b757771bfc9c7465284fd7720ee17d9ed
Renan Lopes <renan-lopes123@hotmail.com>
2018-12-12 11:08:36 -0200

    EndAction

---
+++ ValhallaWay/Assets/EndAction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class EndAction : MonoBehaviour {
+	private void OnTriggerStay2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+		{
+			UnityEngine.SceneManagement.SceneManager.LoadScene ("Thanks");
+		}
+	}
+}
